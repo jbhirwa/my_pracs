ae17f24e69d19df1639ec6d954c4ef450b2f8cd1
john habaguhirwa <johnhabaguhirwa@johns-MacBook-Pro.local>
2024-03-01 23:02:45 +1030

    2.4

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -2,10 +2,13 @@
 
 bool is_ascending(int array[], int n)
 {
-    bool sorted = true;
-    if (n < 1)
-        return (0);
-    sorted = std::is_sorted(array, array + n);
-
-    return (sorted);
+    // bool sorted = true;
+    // if (n < 1)
+    //     return (0);
+    // sorted = std::is_sorted(array, array + n);
+    // return (sorted);
+    for (int i = 0; i < n; i++)
+        if (array[i] <= array[i + 1])
+            return false;
+    return true;
 }
