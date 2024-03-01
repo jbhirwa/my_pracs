b2cd1368f7d2f691e58c3cc190bd04b1824beefe
john habaguhirwa <johnhabaguhirwa@johns-MacBook-Pro.local>
2024-03-02 00:06:20 +1030

    done

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -2,18 +2,27 @@
 
 bool is_fanarray(int array[], int n)
 {
-    bool fan = true;
     if (n < 1)
-        return (false);
-    for (int i = 0; i < n; i++)
-    {
-        if (array[i] == array[n - i - 1])
-            fan = true;
-        else
-        {
-            fan = false;
-            break;
-        }
-    }
-    return fan;
+        return false;
+    for (int i = 0; i < n / 2; i++)
+        if (array[i] != array[n - i - 1])
+            return false;
+    return true;
 }
+
+// {
+//     bool fan = true;
+//     if (n < 1)
+//         return (false);
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (array[i] == array[n - i - 1])
+//             fan = true;
+//         else
+//         {
+//             fan = false;
+//             break;
+//         }
+//     }
+//     return fan;
+// }
--- function-3-3.cpp
+++ function-3-3.cpp
@@ -2,44 +2,43 @@
 
 double weighted_average(int array[], int n)
 {
-    int i = 0;
+    // int i = 0;
     int occur[n];
     double sum = 0;
     if (n < 1)
         return (0);
 
-    // for (int i = 0; i < n; i++)
-    // {
-    //     int k = 0;
-    //     for (int j = 0; j < n; j++)
-    //     {
-    //         if (array[j] == array[i])
-    //             k++;
-    //     }
-    //     occur[i] = k;
-    // }
-
-    while (i < n)
+    for (int i = 0; i < n; i++)
     {
-        int j = 0;
         int k = 0;
-        while (j < n)
+        for (int j = 0; j < n; j++)
         {
             if (array[j] == array[i])
-            {
                 k++;
-                j++;
-            }
-            else
-                j++;
         }
         occur[i] = k;
-        i++;
     }
+
+    // while (i < n)
+    // {
+    //     int j = 0;
+    //     int k = 0;
+    //     while (j < n)
+    //     {
+    //         if (array[j] == array[i])
+    //         {
+    //             k++;
+    //             j++;
+    //         }
+    //         else
+    //             j++;
+    //     }
+    //     occur[i] = k;
+    //     i++;
+    // }
     for (int p = 0; p < n; p++)
     {
         sum += ((double)array[p] * (double)occur[p] / (double)n);
     }
-    // std::cout << sum;
     return (sum);
 }
