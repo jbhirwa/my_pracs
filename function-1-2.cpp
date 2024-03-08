e87761fe11b29215668480a20b60f3261a923b37
john habaguhirwa <johnhabaguhirwa@johns-MacBook-Pro.local>
2024-03-08 23:02:14 +1030

    made changes 12 13

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -2,8 +2,17 @@
 
 int is_identity(int array[10][10])
 {
+    int zeros = 0;
     for (int i = 0; i < 10; i++)
     {
+        for (int j = 0; j < 10; j++)
+        {
+            if (array[i][j] == 0)
+                zeros++;
+        }
+        if (zeros != 9)
+            return 0;
+        zeros = 0;
         if (array[i][i] != 1)
             return 0;
     }
