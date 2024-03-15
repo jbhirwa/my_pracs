b9b4053a13688e467e4e4bca8c76c520b862cb7f
john habaguhirwa <johnhabaguhirwa@student-10-201-05-005.wireless.adelaide.edu.au>
2024-03-15 15:33:04 +1030

    rm aout

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -5,7 +5,5 @@ double *duplicateArray(double *array, int size)
     double *array2 = new double[size];
     for (int i = 0; i < size; i++)
         array2[i] = array[i];
-    // for (int i = 0; i < size; i++)
-    //     std::cout << array2[i];
     return (array2);
 }
