f5ac2eeda60ad587a3e257bb7281d49238804a12
john habaguhirwa <johnhabaguhirwa@johns-MacBook-Pro.local>
2024-03-22 13:15:58 +1030

    h

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,6 +1,20 @@
 #include "Person.h"
 #include <iostream>
 
+PersonList createPersonList(int n)
+{
+    PersonList personlist_;
+    personlist_.numPeople = n;
+    personlist_.people = new Person[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        personlist_.people[i].age = 1;
+        personlist_.people[i].name = "Jane Doe";
+    }
+    return personlist_;
+}
+
 PersonList deepCopyPersonList(PersonList pl)
 {
     PersonList personlist_;
--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include "Person.h"
 
+PersonList createPersonList(int n)
+{
+    PersonList personlist_;
+    personlist_.numPeople = n;
+    personlist_.people = new Person[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        personlist_.people[i].age = 1;
+        personlist_.people[i].name = "Jane Doe";
+    }
+    return personlist_;
+}
+
 PersonList shallowCopyPersonList(PersonList pl)
 {
     return pl;
